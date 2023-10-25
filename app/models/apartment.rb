9763f663a00731cb4bc4ef37c9db5763ab38a553
Shaun Stephenson <shaunfitzgarald@gmail.com>
2023-10-25 09:58:06 -0700

    Finished testing coverage for all fields

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,12 +1,15 @@
 class Apartment < ApplicationRecord
     belongs_to :user
     validates :street, presence: true
-    # validates :unit, presence: true
-    # validates :city, presence: true
-    # validates :state, presence: true
-    # validates :square_footage, numericality: true
-    # validates :price, numericality: true
-    # validates :bedrooms, numericality: { only_integer: true }
-    # validates :bathrooms, numericality: { only_integer: true }
-    # validates :pets, inclusion: { in: ['yes', 'no'] }
+    validates :unit, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :square_footage, numericality: true, presence: true
+    validates :price, presence: true
+    validates :bedrooms, numericality: true, presence: true
+    validates :bathrooms, numericality: true, presence: true
+    validates :pets, presence: true
+    validates :pets, inclusion: { in: ['yes', 'no'] }
+    validates :image, presence: true
+
 end
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -56,6 +56,29 @@ RSpec.describe "Apartments", type: :request do
       expect(response).to have_http_status(200)
     end
 
+    it 'it is not valid if unit is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive' ,
+          unit: nil,
+          city: 'Strike City',
+          state: 'Delulu',
+          square_footage: 2000,
+          price: '4000',
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: 'yes',
+          image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+          user_id: user.id
+        }
+      }
+      post '/apartments', params: apartment_params
+      expect(response.status).to eq(422)
+     
+      apartment = JSON.parse(response.body)
+      expect(apartment['unit']).to include("can't be blank")
+    end
+
     it 'it is not valid if street is empty' do
       apartment_params = {
         apartment: {
@@ -76,12 +99,206 @@ RSpec.describe "Apartments", type: :request do
       # p 'response', response
       expect(response.status).to eq(422)
       # p "Response Body:", JSON.parse(response.body)
-     
+   
       apartment = JSON.parse(response.body)
       expect(apartment['street']).to include("can't be blank")
     end
-
-  end
-
-
+    it 'it is not valid if city is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive' ,
+          unit: '1',
+          city: nil,
+          state: 'Delulu',
+          square_footage: 2000,
+          price: '4000',
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: 'yes',
+          image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+          user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['city']).to include("can't be blank")
+    end
+    
+    it 'it is not valid if state is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive',
+          unit: '1',
+          city: 'Strike City',
+          state: nil,
+          square_footage: 2000,
+          price: '4000',
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: 'yes',
+          image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+          user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['state']).to include("can't be blank")
+    end
+    it 'it is not valid if square_footage is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive',
+          unit: '1',
+          city: 'Strike City',
+          state: 'Delulu',
+          square_footage: nil,
+          price: '4000',
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: 'yes',
+          image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+          user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['square_footage']).to include("can't be blank")
+    end
+    it 'it is not valid if price is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive',
+          unit: '1',
+          city: 'Strike City',
+          state: 'Delulu',
+          square_footage: 2000,
+          price: nil,
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: 'yes',
+          image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+          user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['price']).to include("can't be blank")
+    end
+    it 'it is not valid if bedrooms is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive',
+          unit: '1',
+          city: 'Strike City',
+          state: 'Delulu',
+          square_footage: 2000,
+          price: '4000',
+          bedrooms: nil,
+          bathrooms: 2,
+          pets: 'yes',
+          image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+          user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['bedrooms']).to include("can't be blank")
+    end
+    it 'it is not valid if bathrooms is empty' do
+      apartment_params = {
+        apartment: {
+            street: '1513 Vietnam Drive',
+            unit: '1',
+            city: 'Strike City',
+            state: 'Delulu',
+            square_footage: 2000,
+            price: '4000',
+            bedrooms: 3,
+            bathrooms: nil,
+            pets: 'yes',
+            image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+            user_id: user.id
+          }
+        }
+          post '/apartments', params: apartment_params
+          # p 'response', response
+          expect(response.status).to eq(422)
+          # p "Response Body:", JSON.parse(response.body)
+        
+          apartment = JSON.parse(response.body)
+          expect(apartment['bathrooms']).to include("can't be blank")
+    end
+    it 'it is not valid if pets is empty' do
+      apartment_params = {
+        apartment: {
+        street: '1513 Vietnam Drive',
+        unit: '1',
+        city: 'Strike City',
+        state: 'Delulu',
+        square_footage: 2000,
+        price: '4000',
+        bedrooms: 3,
+        bathrooms: 2,
+        pets: nil,
+        image: 'https://unsplash.com/photos/brown-wooden-house-near-green-trees-during-daytime-hEAGekaiJ8g',
+        user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['pets']).to include("can't be blank", "is not included in the list")
+    end
+    it 'it is not valid if image is empty' do
+      apartment_params = {
+        apartment: {
+          street: '1513 Vietnam Drive',
+          unit: '1',
+          city: 'Strike City',
+          state: 'Delulu',
+          square_footage: 2000,
+          price: '4000',
+          bedrooms: 3,
+          bathrooms: 2,
+          pets: 'yes',
+          image: nil,
+          user_id: user.id
+        }
+      }
+        post '/apartments', params: apartment_params
+        # p 'response', response
+        expect(response.status).to eq(422)
+        # p "Response Body:", JSON.parse(response.body)
+      
+        apartment = JSON.parse(response.body)
+        expect(apartment['image']).to include("can't be blank")
+    end
+  end              
 end
+
+
+
+
